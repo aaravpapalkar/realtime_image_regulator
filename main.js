14bc24ffe59fb66501f60f7385c484aeb4085138
aaravpapalkar <71773470+aaravpapalkar@users.noreply.github.com>
2021-05-11 19:39:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 function setup() {
     canvas = createCanvas(400, 400);
-    canvas.position(575, 200)
+    canvas.position(575, 200);
+    video = createCapture(VIDEO);
+    video.size(400, 400);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("ModelLoaded!");
+}
+
+function draw() {
+
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+    }
 }
