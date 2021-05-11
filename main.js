d95d4c27a76858ca1629f0b3e109604d547a448d
aaravpapalkar <71773470+aaravpapalkar@users.noreply.github.com>
2021-05-11 20:15:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+noseX = 0;
+noseY = 0;
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+
 function setup() {
     canvas = createCanvas(400, 400);
     canvas.position(575, 200);
@@ -12,11 +18,21 @@ function modelLoaded() {
 }
 
 function draw() {
-
+    background("gray");
+    textSize(difference);
+    document.getElementById("square_side").innerHTML = "Size of the text = " + difference + "px";
+    fill("red");
+    text("Aarav", noseX, noseY);
 }
 
 function gotPoses(results) {
     if (results.length > 0) {
-        console.log(results)
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = Math.floor(Number(leftWristX) - Number(rightWristX));
     }
 }
